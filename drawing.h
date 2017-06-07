974bfb2479d33d957f794f7f45fcd6811290c1de
Jesús Miramontes <jesus_miramontes@me.com>
2017-06-07 12:14:17 -0500

    Init commit

---
+++ drawing.h
@@ -0,0 +1,31 @@
+#ifndef DRAWING_H
+#define DRAWING_H
+#include <QPointF>
+#include <QPixmap>
+#include <QPainter>
+#include <QColor>
+
+class drawing
+{
+public:
+    drawing();
+
+    // Lines
+    static void lineBresenham(QPainter* canvas, QPointF* p1, QPointF* p2, int width=3, QColor color=Qt::green);
+    static void lineDDA(QPainter* canvas, QPointF* p1, QPointF* p2, int width=3, QColor color = Qt::green);
+    static void lineBresenhamWithAngle(QPainter* canvas, QPointF* posicion_actual, int angle, int length=100, int width=3, QColor color = Qt::green);
+
+    // Triangles
+    static void triangle(QPainter *canvas, QPointF *p1, QPointF *p2, QPointF *p3, int width=3, QColor color = Qt::green);
+    static void triangle(QPainter* canvas, QPointF* p, int angle, int length=100, int width=3, QColor color = Qt::green);
+
+    // Circle
+    static void circleBresenham(QPainter* canvas, QPointF* point, int r, int width=3, QColor color = Qt::green);
+
+    // Square
+    static void square(QPainter* canvas, QPointF* p1, QPointF* p2, QPointF* p3, QPointF* p4, int width=3, QColor color = Qt::green);
+
+    static QPointF* rotate(QPointF* posicion_actual, int angle, int length=3);
+};
+
+#endif // DRAWING_H
